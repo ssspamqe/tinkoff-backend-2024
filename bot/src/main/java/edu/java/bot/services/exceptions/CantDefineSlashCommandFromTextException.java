19f1f2cd76a7e815a90fe5b0e981dd32fa78c132
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-10 23:49:17 +0300

    Delete CantDefineSlashCommandFromTextException.java
    No more need in this exception

--- bot/src/main/java/edu/java/bot/services/exceptions/CantDefineSlashCommandFromTextException.java
+++
@@ -1,7 +0,0 @@
-package edu.java.bot.services.exceptions;
-
-public class CantDefineSlashCommandFromTextException extends CommandServiceException {
-    public CantDefineSlashCommandFromTextException(String message) {
-        super(message);
-    }
-}
