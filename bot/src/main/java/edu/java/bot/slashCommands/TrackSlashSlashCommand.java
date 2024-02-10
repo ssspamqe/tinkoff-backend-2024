91733288cbab0a9af20e8a9292572e50a7d5cdfb
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-10 14:56:05 +0300

    Add implementation for TrackSlashCommand

--- bot/src/main/java/edu/java/bot/slashCommands/TrackSlashSlashCommand.java
+++ bot/src/main/java/edu/java/bot/slashCommands/TrackSlashSlashCommand.java
@@ -2,7 +2,11 @@ package edu.java.bot.slashCommands;
 
 import com.pengrad.telegrambot.model.BotCommand;
 import com.pengrad.telegrambot.model.Message;
+import edu.java.bot.data.entities.Subscription;
 import edu.java.bot.data.repositories.SubscriptionRepository;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.Validator;
+import java.util.Set;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,14 +14,17 @@ import org.springframework.stereotype.Component;
 public class TrackSlashSlashCommand implements ParameterizedExecutableSlashCommand, NoParametersExecutableSlashCommand {
 
     private static final String TEXT_COMMAND = "/track";
-
     private static final String DESCRIPTION = "Start tracking updates from given link";
+    private static final String LINK_SUCCESSFULLY_ADDED = "Given link was successfully added to /track it!";
+    private static final String INIT_REQUEST_MESSAGE = "Reply to this message with link to /track it!";
 
     private final SubscriptionRepository subscriptionRepository;
+    private final Validator validator;
 
     @Autowired
-    public TrackSlashSlashCommand(SubscriptionRepository subscriptionRepository) {
+    public TrackSlashSlashCommand(SubscriptionRepository subscriptionRepository, Validator validator) {
         this.subscriptionRepository = subscriptionRepository;
+        this.validator = validator;
     }
 
     @Override
@@ -32,16 +39,40 @@ public class TrackSlashSlashCommand implements ParameterizedExecutableSlashComma
 
     @Override
     public String executeAndGetResponse() {
-        return null;
+        return INIT_REQUEST_MESSAGE;
     }
 
     @Override
     public String executeWithParametersAndGetResponse(Message message) {
-        return null;
+        Long userId = message.from().id();
+        String link = message.text();
+        Subscription subscription = new Subscription(0L, userId, link);
+
+        var triggeredViolations = validator.validate(subscription);
+        if (!triggeredViolations.isEmpty()) {
+            return getErrorResponse(triggeredViolations);
+        }
+
+        subscriptionRepository.save(subscription);
+
+        return LINK_SUCCESSFULLY_ADDED;
+    }
+
+    private String getErrorResponse(Set<ConstraintViolation<Subscription>> violations) {
+        var violationList = violations.stream().toList();
+
+        StringBuilder response = new StringBuilder();
+        response.append("Can't track link because:\n");
+        for (int i = 0; i < violationList.size(); i++) {
+            response.append(STR."\{i + 1}) \{violationList.get(i).getMessage()}");
+        }
+
+        return response.toString();
     }
 
     @Override
     public BotCommand getBotCommand() {
         return new BotCommand(TEXT_COMMAND, DESCRIPTION);
     }
+
 }
