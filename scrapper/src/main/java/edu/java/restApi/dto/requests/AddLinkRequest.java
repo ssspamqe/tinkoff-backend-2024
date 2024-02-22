8687b831b2ce31db6ddd2dec84958191ba9c3ac6
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-22 23:07:25 +0300

    Add missing validation to request DTOs

--- scrapper/src/main/java/edu/java/restApi/dto/requests/AddLinkRequest.java
+++ scrapper/src/main/java/edu/java/restApi/dto/requests/AddLinkRequest.java
@@ -1,6 +1,9 @@
 package edu.java.restApi.dto.requests;
 
+import jakarta.validation.constraints.Pattern;
+
 public record AddLinkRequest(
+    @Pattern(regexp = "https?://.*")
     String link
 ) {
 }
--- scrapper/src/main/java/edu/java/restApi/dto/requests/RemoveLinkRequest.java
+++ scrapper/src/main/java/edu/java/restApi/dto/requests/RemoveLinkRequest.java
@@ -1,6 +1,9 @@
 package edu.java.restApi.dto.requests;
 
+import jakarta.validation.constraints.Pattern;
+
 public record RemoveLinkRequest(
+    @Pattern(regexp = "https?://.*")
     String link
 ) {
 }
