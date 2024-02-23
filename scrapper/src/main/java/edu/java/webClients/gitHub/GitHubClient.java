2eb5d13b84fe43d19eda0488be8a21dbac0d0029
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-23 22:36:42 +0300

    Use ResponseEntity<> in webClients
    For retrieving info about http statuses

--- scrapper/src/main/java/edu/java/webClients/gitHub/GitHubClient.java
+++ scrapper/src/main/java/edu/java/webClients/gitHub/GitHubClient.java
@@ -3,6 +3,7 @@ package edu.java.webClients.gitHub;
 import edu.java.webClients.gitHub.dto.GitHubRepository;
 import edu.java.webClients.gitHub.dto.GitHubRepositoryActivity;
 import java.util.List;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -11,8 +12,11 @@ import org.springframework.web.service.annotation.HttpExchange;
 public interface GitHubClient {
 
     @GetExchange(url = "/repos/{owner}/{repoName}")
-    GitHubRepository findRepository(@PathVariable String owner, @PathVariable String repoName);
+    ResponseEntity<GitHubRepository> findRepository(@PathVariable String owner, @PathVariable String repoName);
 
     @GetExchange(url = "/repos/{owner}/{repoName}/activity")
-    List<GitHubRepositoryActivity> findRepositoryActivities(@PathVariable String owner, @PathVariable String repoName);
+    ResponseEntity<List<GitHubRepositoryActivity>> findRepositoryActivities(
+        @PathVariable String owner,
+        @PathVariable String repoName
+    );
 }
--- scrapper/src/main/java/edu/java/webClients/stackOverflow/StackOverflowClient.java
+++ scrapper/src/main/java/edu/java/webClients/stackOverflow/StackOverflowClient.java
@@ -3,6 +3,7 @@ package edu.java.webClients.stackOverflow;
 import edu.java.webClients.stackOverflow.dto.StackOverflowAnswer;
 import edu.java.webClients.stackOverflow.dto.StackOverflowQuestion;
 import edu.java.webClients.stackOverflow.dto.StackOverflowResponse;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.service.annotation.GetExchange;
 import org.springframework.web.service.annotation.HttpExchange;
@@ -10,9 +11,9 @@ import org.springframework.web.service.annotation.HttpExchange;
 @HttpExchange
 public interface StackOverflowClient {
     @GetExchange(url = "/questions/{id}?site=stackoverflow&filter=withbody")
-    StackOverflowResponse<StackOverflowQuestion> findQuestionById(@PathVariable int id);
+    ResponseEntity<StackOverflowResponse<StackOverflowQuestion>> findQuestionById(@PathVariable int id);
 
     @GetExchange(url = "/questions/{id}/answers?site=stackoverflow&filter=withbody")
-    StackOverflowResponse<StackOverflowAnswer> findAnswersByQuestionId(@PathVariable int id);
+    ResponseEntity<StackOverflowResponse<StackOverflowAnswer>> findAnswersByQuestionId(@PathVariable int id);
 
 }
