5871159f95ccc5a7af0324ce787a5140bc549294
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-25 02:38:23 +0300

    Use UUID in LinkRepository
    Since Link entity uses UUID

--- scrapper/src/main/java/edu/java/data/repositories/LinkRepository.java
+++ scrapper/src/main/java/edu/java/data/repositories/LinkRepository.java
@@ -1,9 +1,12 @@
 package edu.java.data.repositories;
 
 import edu.java.data.entities.Link;
+import java.util.Optional;
+import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface LinkRepository extends CrudRepository<Link, Long> {
+public interface LinkRepository extends CrudRepository<Link, UUID> {
+    Optional<Link> findByUrl(String url);
 }
