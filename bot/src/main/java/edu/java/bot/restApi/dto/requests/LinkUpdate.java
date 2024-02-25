57c95021746d8175dd8c2d456ef6b76b9af9cb45
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-26 01:44:42 +0300

    Use URI for links in rest api DTOs

--- bot/src/main/java/edu/java/bot/restApi/dto/requests/LinkUpdate.java
+++ bot/src/main/java/edu/java/bot/restApi/dto/requests/LinkUpdate.java
@@ -2,6 +2,7 @@ package edu.java.bot.restApi.dto.requests;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotEmpty;
+import java.net.URI;
 import java.util.List;
 
 public record LinkUpdate(
@@ -9,7 +10,7 @@ public record LinkUpdate(
     int id,
 
     @NotBlank
-    String url,
+    URI url,
 
     @NotBlank
     String description,
--- scrapper/src/main/java/edu/java/restApi/controllers/dto/requests/AddLinkRequest.java
+++ scrapper/src/main/java/edu/java/restApi/controllers/dto/requests/AddLinkRequest.java
@@ -1,9 +1,10 @@
 package edu.java.restApi.controllers.dto.requests;
 
 import jakarta.validation.constraints.Pattern;
+import java.net.URI;
 
 public record AddLinkRequest(
     @Pattern(regexp = "https?://.*")
-    String link
+    URI link
 ) {
 }
