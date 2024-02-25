dcc91b3ae688dd8aaa735330ac5b9856334d0975
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-26 01:04:39 +0300

    Chane RedisTemplate Bean
    Set up key and value and key serealization

--- scrapper/src/main/java/edu/java/configuration/RedisConfig.java
+++ scrapper/src/main/java/edu/java/configuration/RedisConfig.java
@@ -5,8 +5,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
+import org.springframework.data.redis.serializer.GenericToStringSerializer;
+import redis.clients.jedis.Jedis;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
     private final ApplicationConfig applicationConfig;
@@ -17,17 +22,20 @@ public class RedisConfig {
     }
 
     @Bean
+    public RedisTemplate<String, Object> redisTemplate() {
+        final RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(jedisConnectionFactory());
+        template.setKeySerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.setValueSerializer(new GenericToStringSerializer<Object>(Object.class));
+        template.afterPropertiesSet();
+        return template;
+    }
+
+    @Bean
     public JedisConnectionFactory jedisConnectionFactory() {
         JedisConnectionFactory factory = new JedisConnectionFactory();
         factory.setHostName(applicationConfig.redisConnection().hostName());
         factory.setPort(applicationConfig.redisConnection().port());
         return factory;
-    }
-
-    @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
-        RedisTemplate<String, Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(jedisConnectionFactory());
-        return template;
     }
 }
