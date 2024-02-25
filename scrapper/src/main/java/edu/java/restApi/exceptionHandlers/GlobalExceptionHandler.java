a1a7a62306a0db101a25c1493caaf21013fc39fe
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-02-26 01:40:15 +0300

    Add exceptions to GlobalExceptionHandler

--- scrapper/src/main/java/edu/java/restApi/exceptionHandlers/GlobalExceptionHandler.java
+++ scrapper/src/main/java/edu/java/restApi/exceptionHandlers/GlobalExceptionHandler.java
@@ -1,6 +1,11 @@
 package edu.java.restApi.exceptionHandlers;
 
 import edu.java.restApi.controllers.dto.responses.ApiErrorResponse;
+import edu.java.restApi.services.exceptions.DoubleChatRegistrationException;
+import edu.java.restApi.services.exceptions.NoSuchChatException;
+import java.util.Arrays;
+import java.util.List;
+import edu.java.restApi.services.exceptions.NoSuchLinkException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.http.HttpStatusCode;
@@ -8,32 +13,65 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import java.util.Arrays;
-import java.util.List;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     private static final Logger LOGGER = LogManager.getLogger();
     private static final String INCORRECT_REQUEST_PARAMETERS_DESCRIPTION = "Incorrect request parameters";
+    private static final String DOUBLE_CHAT_REGISTRATION_DESCRIPTION =
+        "Can't register new chat, as it was already registered";
+    private static final String NO_SUCH_CHAT_DESCRIPTION = "There is no record about such chat";
+    private static final String NO_SUCH_LINK_DESCRIPTION = "There is no record about such link";
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ApiErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
         HttpStatusCode statusCode = exception.getStatusCode();
+        ApiErrorResponse errorResponse =
+            buildDefaultErrorResponse(statusCode, INCORRECT_REQUEST_PARAMETERS_DESCRIPTION, exception);
+
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    @ExceptionHandler(DoubleChatRegistrationException.class)
+    public ResponseEntity<ApiErrorResponse> handleDoubleChatRegistrationException(DoubleChatRegistrationException exception) {
+        HttpStatusCode statusCode = HttpStatusCode.valueOf(409);
+        ApiErrorResponse errorResponse =
+            buildDefaultErrorResponse(statusCode, DOUBLE_CHAT_REGISTRATION_DESCRIPTION, exception);
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchChatException.class)
+    public ResponseEntity<ApiErrorResponse> handleNoSuchChatException(NoSuchChatException exception) {
+        HttpStatusCode statusCode = HttpStatusCode.valueOf(404);
+        ApiErrorResponse errorResponse =
+            buildDefaultErrorResponse(statusCode, NO_SUCH_CHAT_DESCRIPTION, exception);
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    @ExceptionHandler(NoSuchLinkException.class)
+    public ResponseEntity<ApiErrorResponse> handleNoSuchLinkException(NoSuchLinkException exception) {
+        HttpStatusCode statusCode = HttpStatusCode.valueOf(404);
+        ApiErrorResponse errorResponse =
+            buildDefaultErrorResponse(statusCode, NO_SUCH_LINK_DESCRIPTION, exception);
+        return ResponseEntity.status(statusCode).body(errorResponse);
+    }
+
+    private ApiErrorResponse buildDefaultErrorResponse(
+        HttpStatusCode statusCode,
+        String description,
+        Exception exception
+    ) {
         String exceptionName = exception.getClass().getSimpleName();
         String exceptionMessage = exception.getMessage();
         List<String> stacktrace = Arrays.stream(exception.getStackTrace()).map(StackTraceElement::toString).toList();
 
-        ApiErrorResponse errorResponse = new ApiErrorResponse(
-            INCORRECT_REQUEST_PARAMETERS_DESCRIPTION,
+        return new ApiErrorResponse(
+            description,
             statusCode.toString(),
             exceptionName,
             exceptionMessage,
             stacktrace
         );
-
-        LOGGER.warn("Incorrect request parameters: {}", errorResponse);
-        return ResponseEntity.status(statusCode).body(errorResponse);
     }
-
 }
