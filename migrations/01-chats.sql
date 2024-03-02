276f076453f1b78fa8c0457e8e383d18f2537cb6
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-03 02:42:02 +0300

    Make api_id not null

--- migrations/01-chats.sql
+++ migrations/01-chats.sql
@@ -1,7 +1,7 @@
 create table chats
 (
     id         bigint generated always as identity,
-    api_id     bigint,
+    api_id     bigint                   not null,
     created_at timestamp with time zone not null,
 
     primary key (id)
