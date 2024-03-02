f8e750daee110dcca60652f887076f03653a6fe3
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-03 01:41:16 +0300

    Reformat .sql files

--- migrations/01-chats.sql
+++ migrations/01-chats.sql
@@ -1,5 +1,7 @@
-create table chats(
-    id bigint not null,
+create table chats
+(
+    id         bigint generated always as identity,
+    api_id     bigint,
     created_at timestamp with time zone not null,
 
     primary key (id)
--- scrapper/src/test/resources/migrations/03-chat_links.sql
+++ scrapper/src/test/resources/migrations/03-chat_links.sql
@@ -1,5 +1,6 @@
-create table chat_links(
-    id bigint generated always as identity ,
-    chat_id bigint not null references chats(id),
-    link_id bigint not null references links(id)
+create table chat_links
+(
+    id      bigint generated always as identity,
+    chat_id bigint not null references chats (id),
+    link_id bigint not null references links (id)
 )
