4de8b16c486a9701c274dc5cb00817ca5b20fefc
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-07 22:19:09 +0300

    Use @ServiceConnection for posgres test container

--- scrapper/src/test/java/edu/java/scrapper/integrationTests/IntegrationTest.java
+++ scrapper/src/test/java/edu/java/scrapper/integrationTests/IntegrationTest.java
@@ -11,15 +11,18 @@ import liquibase.database.DatabaseFactory;
 import liquibase.database.jvm.JdbcConnection;
 import liquibase.resource.DirectoryResourceAccessor;
 import liquibase.resource.ResourceAccessor;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.JdbcDatabaseContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 @Testcontainers
 public abstract class IntegrationTest {
-    public static PostgreSQLContainer<?> POSTGRES;
+
+    @Container
+    @ServiceConnection
+    static PostgreSQLContainer<?> POSTGRES;
 
     static {
         POSTGRES = new PostgreSQLContainer<>("postgres:16")
@@ -27,13 +30,11 @@ public abstract class IntegrationTest {
             .withUsername("postgres")
             .withPassword("postgres");
         POSTGRES.start();
-
         try {
             runMigrations(POSTGRES);
         } catch (Exception ex) {
             throw new RuntimeException(ex);
         }
-
     }
 
     private static void runMigrations(JdbcDatabaseContainer<?> c) throws Exception {
@@ -46,12 +47,5 @@ public abstract class IntegrationTest {
         ResourceAccessor resourceAccessor = new DirectoryResourceAccessor(changeLogPath);
         Liquibase liquibase = new Liquibase("master.yaml", resourceAccessor, database);
         liquibase.update(new Contexts(), new LabelExpression());
-    }
-
-    @DynamicPropertySource
-    static void jdbcProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", POSTGRES::getJdbcUrl);
-        registry.add("spring.datasource.username", POSTGRES::getUsername);
-        registry.add("spring.datasource.password", POSTGRES::getPassword);
     }
 }
