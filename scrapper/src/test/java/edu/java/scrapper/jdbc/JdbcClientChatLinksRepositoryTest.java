e9aecfc2b0083489615b7f9b4b93173b562c7f0d
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-09 16:37:01 +0300

    Fix tests for JdbcClientChatLInksRepository

--- scrapper/src/test/java/edu/java/scrapper/jdbc/JdbcClientChatLinksRepositoryTest.java
+++ scrapper/src/test/java/edu/java/scrapper/jdbc/JdbcClientChatLinksRepositoryTest.java
@@ -16,23 +16,29 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
+//TODO use transaction for rollback
 public class JdbcClientChatLinksRepositoryTest extends JdbcIntegrationEnvironment {
 
     @Autowired @Qualifier("jdbcClientChatLinksRepository")
     ChatLinksRepository chatLinksRepository;
 
     Connection connection;
+    Statement statement;
 
     @BeforeEach
     void beforeEach() throws SQLException {
         connection =
             DriverManager.getConnection(POSTGRES.getJdbcUrl(), POSTGRES.getUsername(), POSTGRES.getPassword());
+        statement = connection.createStatement();
     }
 
     @AfterEach
     void afterEach() throws SQLException {
-        Statement statement = connection.createStatement();
-        statement.execute("TRUNCATE chat_links");
+        statement.execute("TRUNCATE chat_links RESTART IDENTITY ");
+        statement.execute("TRUNCATE chats RESTART IDENTITY CASCADE");
+        statement.execute("TRUNCATE links RESTART IDENTITY CASCADE");
+
+        statement.close();
         connection.close();
     }
 
@@ -45,48 +51,60 @@ public class JdbcClientChatLinksRepositoryTest extends JdbcIntegrationEnvironmen
 
     @Test
     public void should_findByChatId() throws SQLException {
-        Statement statement = connection.createStatement();
+        //Arrange
+        statement.execute("INSERT INTO chats (telegram_api_id, created_at) VALUES (11,'2022-06-16 16:37:23')");
+        statement.execute("INSERT INTO chats (telegram_api_id, created_at) VALUES (12,'2022-06-16 16:37:23')");
+
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('https://link1','2022-06-16 16:37:23')");
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('https://link2','2022-06-16 16:37:23')");
+
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,1)");
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,2)");
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (2,1)");
 
+        //Act
         List<Long> actualLinkIds = chatLinksRepository.findByChatId(1)
             .stream().map(ChatLink::getLinkId).toList();
 
+        //Assert
         assertThat(actualLinkIds).isNotNull().hasSize(2).containsExactlyInAnyOrder(1L, 2L);
     }
 
     @Test
     public void should_findByLinkId() throws SQLException {
-        Statement statement = connection.createStatement();
+        //Arrange
+        statement.execute("INSERT INTO chats (telegram_api_id, created_at) VALUES (11,'2022-06-16 16:37:23')");
+        statement.execute("INSERT INTO chats (telegram_api_id, created_at) VALUES (12,'2022-06-16 16:37:23')");
+
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('https://link1','2022-06-16 16:37:23')");
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('https://link2','2022-06-16 16:37:23')");
+
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,1)");
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (2,1)");
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,2)");
 
+        //Act
         List<Long> actualChatIds = chatLinksRepository.findByLinkId(1)
             .stream().map(ChatLink::getChatId).toList();
 
+        //Assert
         assertThat(actualChatIds).isNotNull().hasSize(2).containsExactlyInAnyOrder(1L, 2L);
     }
 
     @Test
     public void should_removeByChatIdAndLinkId() throws SQLException {
-        //Assert
-        Statement statement = connection.createStatement();
+        //Arrange
+        statement.execute("INSERT INTO chats (telegram_api_id, created_at) VALUES (11,'2022-06-16 16:37:23')");
+
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('https://link1','2022-06-16 16:37:23')");
+
         statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,1)");
-        statement.execute("INSERT INTO chat_links (chat_id, link_id) VALUES (1,2)");
 
         //Act
         chatLinksRepository.removeByChatIdAndLinkId(1, 1);
 
         //Assert
-        ResultSet resultSet = statement.executeQuery("SELECT * FROM links");
-        assertThat(resultSet.getInt("recordCount")).isEqualTo(1);
-
-        resultSet.next();
-        long actualChatId = resultSet.getLong("chat_id");
-        long actualLinkId = resultSet.getLong("link_id");
-        assertThat(actualLinkId).isEqualTo(1);
-        assertThat(actualChatId).isEqualTo(2);
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM chat_links");
+        assertThat(resultSet.next()).isFalse();
     }
 }
