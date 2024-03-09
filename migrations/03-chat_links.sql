2833d1740fb1660ba4ada8667e55b72aa4682a86
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-09 16:32:45 +0300

    Use on delete cascade in database

--- migrations/03-chat_links.sql
+++ migrations/03-chat_links.sql
@@ -1,7 +1,7 @@
 create table chat_links
 (
     id      bigint generated always as identity,
-    chat_id bigint not null references chats (id),
-    link_id bigint not null references links (id),
+    chat_id bigint not null references chats (id) on delete  cascade ,
+    link_id bigint not null references links (id) on delete  cascade ,
     primary key (id)
 )
