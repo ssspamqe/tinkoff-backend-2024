0802e047494efcc8e508c3f3f8ec8a9633b0882c
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-10 15:59:25 +0300

    Add tests for ClientLinkRepository

---
+++ scrapper/src/test/java/edu/java/scrapper/jdbc/JdbcClientLinkRepositoryTest.java
@@ -0,0 +1,62 @@
+package edu.java.scrapper.jdbc;
+
+import com.opencsv.ResultSetHelperService;
+import edu.java.dao.postgres.entities.Link;
+import edu.java.dao.postgres.repositories.LinkRepository;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class JdbcClientLinkRepositoryTest extends JdbcIntegrationEnvironment {
+    @Autowired @Qualifier("jdbcClientLinkRepository")
+    LinkRepository linkRepository;
+
+    @Test
+    public void should_notThrowException_when_saving() {
+        Link link = new Link("some url", LocalDateTime.now());
+
+        assertThatCode(() -> linkRepository.save(link)).doesNotThrowAnyException();
+    }
+
+    @Test
+    public void should_findByLinkId() throws SQLException {
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('some url','2022-06-16 16:37:23')");
+
+        Link actualLink = linkRepository.findById(1L).get();
+
+        assertThat(actualLink.getUrl()).isEqualTo("some url");
+    }
+
+    @Test
+    public void should_returnEmptyOptional_when_cantFindLink() {
+        Optional<Link> link = linkRepository.findById(1L);
+
+        assertThat(link.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void should_findByUrl() throws SQLException {
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('some url','2022-06-16 16:37:23')");
+
+        Link link = linkRepository.findByUrl("some url").get();
+
+        assertThat(link.getCreatedAt()).isEqualTo(LocalDateTime.parse("2022-06-16 16:37:23"));
+    }
+
+    @Test
+    public void should_removeById() throws SQLException {
+        statement.execute("INSERT INTO links (url, created_at) VALUES ('some url','2022-06-16 16:37:23')");
+
+        linkRepository.removeById(1L);
+
+        ResultSet resultSet = statement.executeQuery("SELECT * FROM links");
+        assertThat(resultSet.next()).isFalse();
+    }
+
+}
