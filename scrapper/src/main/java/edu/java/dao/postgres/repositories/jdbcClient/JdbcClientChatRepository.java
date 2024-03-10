1fa49dde8465e4c7275365fc86affc046666cd7f
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-10 17:39:55 +0300

    Add chatRepository Implementation

---
+++ scrapper/src/main/java/edu/java/dao/postgres/repositories/jdbcClient/JdbcClientChatRepository.java
@@ -0,0 +1,73 @@
+package edu.java.dao.postgres.repositories.jdbcClient;
+
+import edu.java.dao.postgres.entities.Chat;
+import edu.java.dao.postgres.repositories.ChatRepository;
+import edu.java.dao.postgres.repositories.jdbcClient.rowMappers.ChatRowMapper;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.simple.JdbcClient;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class JdbcClientChatRepository implements ChatRepository {
+
+    private static final String TABLE_NAME = "chats";
+    private static final RowMapper<Chat> ROW_MAPPER = new ChatRowMapper();
+
+    private static final String SAVE_QUERY =
+        STR."INSERT INTO \{TABLE_NAME} (telegram_api_id, created_at) VALUES (:telegram_api_id, :created_at)";
+
+    private static final String FIND_BY_ID_QUERY =
+        STR."SELECT * FROM \{TABLE_NAME} WHERE id = :id";
+
+    private static final String FIND_BY_TELEGRAM_API_ID_QUERY =
+        STR."SELECT * FROM \{TABLE_NAME} WHERE telegram_api_id = :telegram_api_id";
+
+    private static final String DELETE_BY_ID_QUERY =
+        STR."DELETE FROM \{TABLE_NAME} WHERE id = :id";
+
+    private static final String DELETE_BY_TELEGRAM_API_ID_QUERY =
+        STR."DELETE FROM \{TABLE_NAME} WHERE telegram_api_id = :telegram_api_id";
+
+    private final JdbcClient jdbcClient;
+
+    @Override
+    public void save(Chat chat) {
+        jdbcClient.sql(SAVE_QUERY)
+            .param("telegram_api_id", chat.getTelegramApiId())
+            .param("created_at", chat.getCreatedAt())
+            .update();
+    }
+
+    @Override
+    public Optional<Chat> findById(long id) {
+        return jdbcClient.sql(FIND_BY_ID_QUERY)
+            .param("id", id)
+            .query(ROW_MAPPER)
+            .optional();
+    }
+
+    @Override
+    public Optional<Chat> findByTelegramApiId(long telegramApiId) {
+        return jdbcClient.sql(FIND_BY_TELEGRAM_API_ID_QUERY)
+            .param("telegram_api_id", telegramApiId)
+            .query(ROW_MAPPER)
+            .optional();
+    }
+
+    @Override
+    public void removeById(long id) {
+        jdbcClient.sql(DELETE_BY_ID_QUERY)
+            .param("id", id)
+            .update();
+    }
+
+    @Override
+    public void removeByTelegramApiId(long telegramApiId) {
+        jdbcClient.sql(DELETE_BY_TELEGRAM_API_ID_QUERY)
+            .param("telegram_api_id", telegramApiId)
+            .update();
+    }
+}
