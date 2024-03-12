d8c6f5dfe9c81118b4af238dddff453a6a94c45f
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-12 17:29:17 +0300

    Add full URL regex to requests validation

--- scrapper/src/main/java/edu/java/restApi/controllers/dto/requests/AddLinkRequest.java
+++ scrapper/src/main/java/edu/java/restApi/controllers/dto/requests/AddLinkRequest.java
@@ -3,7 +3,7 @@ package edu.java.restApi.controllers.dto.requests;
 import jakarta.validation.constraints.Pattern;
 
 public record AddLinkRequest(
-    @Pattern(regexp = "https?://.*")
+    @Pattern(regexp = "https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)")
     String url
 ) {
 }
