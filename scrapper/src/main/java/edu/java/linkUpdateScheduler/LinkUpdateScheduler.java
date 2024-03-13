a383c1bb2172648aaac3164b86ff4194aff571c6
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-13 23:28:08 +0300

    Add enable property checking for Scheduler

--- scrapper/src/main/java/edu/java/linkUpdateScheduler/LinkUpdateScheduler.java
+++ scrapper/src/main/java/edu/java/linkUpdateScheduler/LinkUpdateScheduler.java
@@ -2,6 +2,7 @@ package edu.java.linkUpdateScheduler;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,7 +11,8 @@ public class LinkUpdateScheduler {
 
     private static final Logger LOGGER = LogManager.getLogger();
 
-    @Scheduled(fixedDelayString = "#{scheduler.forceCheckDelay()}")
+    @Scheduled(fixedDelayString = "#{scheduler.interval()}")
+    @ConditionalOnProperty(value = "app.scheduler.enable", havingValue = "true")
     public void update() {
         LOGGER.debug("Looking for updates...");
     }
