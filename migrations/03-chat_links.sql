d611b408074f79c75610b57fbbad6d6b203a6b7b
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-21 21:37:12 +0300

    Add default value to sql chat_links(created_at)

--- migrations/03-chat_links.sql
+++ migrations/03-chat_links.sql
@@ -3,7 +3,7 @@ CREATE TABLE chat_links
     id         BIGINT GENERATED ALWAYS AS IDENTITY,
     chat_id    BIGINT    NOT NULL REFERENCES chats (id) ON DELETE CASCADE,
     link_id    BIGINT    NOT NULL REFERENCES links (id) ON DELETE CASCADE,
-    created_at TIMESTAMP NOT NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT '1970-01-01 00:00:00',
 
     PRIMARY KEY (id),
     UNIQUE (chat_id, link_id)
