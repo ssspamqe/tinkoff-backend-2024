f7639cc8b479e48eca0e8747f93750b0d09cc28f
ssspamqe <80048504+ssspamqe@users.noreply.github.com>
2024-03-23 03:25:04 +0300

    Add tests for jooq chat and link DAOs

---
+++ scrapper/src/test/java/edu/java/scrapper/integrational/dao/jooq/ChatJooqDaoTest.java
@@ -0,0 +1,103 @@
+package edu.java.scrapper.integrational.dao.jooq;
+
+import edu.java.data.dao.jdbc.repositories.rowMappers.LinkRowMapper;
+import edu.java.data.dto.Link;
+import edu.java.data.exceptions.NoSuchLinkException;
+import edu.java.scrapper.integrational.DatabaseIntegrationEnvironment;
+import java.net.URI;
+import java.util.Set;
+import java.util.stream.Collectors;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.RowMapper;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class ChatJooqDaoTest extends DatabaseIntegrationEnvironment {
+    private static final RowMapper<Link> LINK_JDBC_ROW_MAPPER = new LinkRowMapper();
+
+    @Test
+    void should_returnTrackedLinksByChatId() {
+        //Arrange
+        saveChatWithId(1);
+
+        long link1Id = saveLinkWithUrl("https://example1.com");
+        long link2Id = saveLinkWithUrl("https://example2.com");
+
+        saveChatIdLinkId(1, link1Id);
+        saveChatIdLinkId(1, link2Id);
+
+        //Act
+        Set<String> actualLinkUrls = chatJdbcDao
+            .getTrackedLinksByChatId(1).stream()
+            .map(link -> link.getUrl().toString())
+            .collect(Collectors.toSet());
+
+        //Assert
+        assertThat(actualLinkUrls).containsExactlyInAnyOrder("https://example1.com", "https://example2.com");
+    }
+
+    @Test
+    void should_associateUrlWithChat_when_noLinkWithSuchUrl() {
+        saveChatWithId(1);
+        URI url = URI.create("https://example.org");
+
+        chatJdbcDao.associateUrlByChatId(url, 1);
+
+        Link savedLink = jdbcTemplate.queryForObject("SELECT * FROM links", LINK_JDBC_ROW_MAPPER);
+        int chatLinksCouplesCount = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM chat_links WHERE chat_id = ? AND link_id = ?",
+            Integer.class,
+            1,
+            savedLink.getId()
+        );
+        assertThat(savedLink.getUrl()).hasToString("https://example.org");
+        assertThat(chatLinksCouplesCount).isEqualTo(1);
+    }
+
+    @Test
+    void should_associateUrlWithChat_when_linkWithSuchUrlExists() {
+        saveChatWithId(1);
+        long linkId = saveLinkWithUrl("https://example.org");
+
+        chatJdbcDao.associateUrlByChatId(URI.create("https://example.org"), 1);
+
+        int chatLinksCouplesCount = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM chat_links WHERE chat_id = ? AND link_id = ?",
+            Integer.class,
+            1,
+            linkId
+        );
+        assertThat(chatLinksCouplesCount).isEqualTo(1);
+    }
+
+    @Test
+    void should_dissociateUrlWithChat_when_linkWithSuchUrlExists() {
+        saveChatWithId(1);
+        long linkId = saveLinkWithUrl("https://example.org");
+        saveChatIdLinkId(1, linkId);
+
+        chatJdbcDao.dissociateUrlByChatId(URI.create("https://example.org"), 1);
+
+        int chatLinksCouplesCount = jdbcTemplate.queryForObject(
+            "SELECT COUNT(*) FROM chat_links WHERE chat_id = ? AND link_id = ?",
+            Integer.class,
+            1,
+            linkId
+        );
+        assertThat(chatLinksCouplesCount).isEqualTo(0);
+    }
+
+    @Test
+    void should_throwException_when_dissociatingUnsavedUrlWithChat() {
+        saveChatWithId(1);
+
+        assertThatThrownBy(() -> chatJdbcDao.dissociateUrlByChatId(URI.create("https://unknow/url"), 1))
+            .isInstanceOf(NoSuchLinkException.class);
+    }
+
+    @Test
+    void should_throwException_when_noSuchChat() {
+        assertThatThrownBy(() -> chatJdbcDao.associateUrlByChatId(URI.create("https://example.com"), 1));
+
+    }
+}
---
+++ scrapper/src/test/java/edu/java/scrapper/integrational/dao/jooq/LinkJooqDaoTest.java
@@ -0,0 +1,90 @@
+package edu.java.scrapper.integrational.dao.jooq;
+
+import edu.java.data.dao.jdbc.repositories.rowMappers.LinkRowMapper;
+import edu.java.data.dto.Link;
+import edu.java.scrapper.integrational.DatabaseIntegrationEnvironment;
+import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.core.RowMapper;
+import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LinkJooqDaoTest extends DatabaseIntegrationEnvironment {
+    private static final RowMapper<Link> LINK_JDBC_MAPPER = new LinkRowMapper();
+
+    @Test
+    void should_findByUrl() {
+        saveLinkWithUrl("https://example.org");
+
+        Optional<Link> actualLink = linkJdbcDao.findByUrl(URI.create("https://example.org"));
+
+        assertThat(actualLink).isPresent();
+    }
+
+    @Test
+    void should_beAbleToSave_when_noLinkWithSuchUrl() {
+        Link link = linkJdbcDao.saveOrFindByUrl(URI.create("https://example.org"));
+
+        int allSavedLinksCount =
+            jdbcTemplate.queryForObject("SELECT COUNT(*) FROM links WHERE url = 'https://example.org'", Integer.class);
+        assertThat(allSavedLinksCount).isEqualTo(1);
+    }
+
+    @Test
+    void should_beAbleToFind_when_linkWithSuchUrlExists() {
+        saveLinkWithUrl("https://example.org");
+
+        Link link = linkJdbcDao.saveOrFindByUrl(URI.create("https://example.org"));
+
+        assertThat(link).isNotNull();
+        assertThat(link.getUrl()).hasToString("https://example.org");
+    }
+
+    @Test
+    void should_findAssociatedChatsByLinkId() {
+        //Assert
+        saveChatWithId(1);
+        saveChatWithId(2);
+
+        long linkId = saveLinkWithUrl("https://example.org");
+
+        saveChatIdLinkId(1, linkId);
+        saveChatIdLinkId(2, linkId);
+
+        //Act
+        Set<Long> associatedChatsIds = linkJdbcDao.findAssociatedChatsIdsByLinkId(linkId);
+
+        assertThat(associatedChatsIds).containsExactlyInAnyOrder(1L, 2L);
+
+    }
+
+    @Test
+    void should_updateLastChecked() {
+        long linkId = saveLinkWithUrl("https://example.org");
+        LocalDateTime expectedLastChecked = LocalDateTime.of(2000, 1, 1, 1, 1);
+
+        linkJdbcDao.updateLastCheckedAtById(expectedLastChecked, linkId);
+
+        Link savedLink = jdbcTemplate.queryForObject("SELECT * FROM links", LINK_JDBC_MAPPER);
+        assertThat(savedLink.getLastCheckedAt()).isEqualTo(expectedLastChecked);
+    }
+
+    @Test
+    void should_findByLastCheckedAtBefore() {
+        jdbcTemplate.update("INSERT INTO links(url, created_at, last_checked_at) VALUES " +
+            "('https://example.org', '1970-01-01 00:00:00', '1980-01-01 00:00:00'), " +
+            "('https://example2.org', '1970-01-01 00:00:00', '1990-01-01 00:00:00'), " +
+            "('https://example3.org', '1970-01-01 00:00:00', '2000-01-01 00:00:00')");
+        LocalDateTime borderDateTime = LocalDateTime.of(1996, 1, 1, 0, 0);
+
+        Set<String> actualLinkUrls = linkJdbcDao
+            .findByLastCheckedAtBefore(borderDateTime).stream()
+            .map(i -> i.getUrl().toString())
+            .collect(Collectors.toSet());
+
+        assertThat(actualLinkUrls).containsExactlyInAnyOrder("https://example.org", "https://example2.org");
+    }
+}
